aa89e5232f7ec8c4b380854b459c743a4361a5fb
Jan Dvorak <dvorakj@gmail.com>
2021-09-28 21:34:54 +0200

    feat: woking ugly animals config

--- src/__tests__/song.test.ts
+++ src/__tests__/song.test.ts
@@ -3,9 +3,9 @@ import { getSong } from "../song";
 describe("song", () => {
   describe("getSong", () => {
     it("returns some string", () => {
-      const actual = getSong();
-
-      expect(typeof actual).toBe("string");
+      const actual = getSong(['fly', 'spider', 'bird', 'cat', 'dog', 'cow']);
+      
+      expect(actual).toMatchSnapshot();
     });
   });
 });
--- src/song.ts
+++ src/song.ts
@@ -1,42 +1,28 @@
-export const getSong = () => `
-  There was an old lady who swallowed a fly.
-  I don't know why she swallowed a fly - perhaps she'll die!
+export const getSong = (animals: string[]) => {
+  const prev = []
+  const rows = animals.reduce((rows, animal, index) => {
+    prev.push(animal)
+    return [...rows, ...getVers(index, animal, prev.slice(0, -1)), '']
+  }, [])
+  rows.push('There was an old lady who swallowed a horse...')
+  rows.push(`...She's dead, of course!`)
+  return rows.join('\n').trim()
+}
 
-  There was an old lady who swallowed a spider;
-  That wriggled and wiggled and tickled inside her.
-  She swallowed the spider to catch the fly;
-  I don't know why she swallowed a fly - perhaps she'll die!
+const HLASKY = ['','That wriggled and wiggled and tickled inside her.', 'How absurd to swallow a bird.', 'Fancy that to swallow a cat!', 'What a hog, to swallow a dog!', `I don't know how she swallowed a cow!`]
 
-  There was an old lady who swallowed a bird;
-  How absurd to swallow a bird.
-  She swallowed the bird to catch the spider,
-  She swallowed the spider to catch the fly;
-  I don't know why she swallowed a fly - perhaps she'll die!
+function getVers(index, animal, previousAnimals): string[] {
+  const rows = [`There was an old lady who swallowed a ${animal}${index === 0 ? '.' : ';'}`]
+  
+  const hlaska = HLASKY[index]
+  hlaska && rows.push(hlaska)
 
-  There was an old lady who swallowed a cat;
-  Fancy that to swallow a cat!
-  She swallowed the cat to catch the bird,
-  She swallowed the bird to catch the spider,
-  She swallowed the spider to catch the fly;
-  I don't know why she swallowed a fly - perhaps she'll die!
-
-  There was an old lady who swallowed a dog;
-  What a hog, to swallow a dog!
-  She swallowed the dog to catch the cat,
-  She swallowed the cat to catch the bird,
-  She swallowed the bird to catch the spider,
-  She swallowed the spider to catch the fly;
-  I don't know why she swallowed a fly - perhaps she'll die!
-
-  There was an old lady who swallowed a cow;
-  I don't know how she swallowed a cow!
-  She swallowed the cow to catch the dog,
-  She swallowed the dog to catch the cat,
-  She swallowed the cat to catch the bird,
-  She swallowed the bird to catch the spider,
-  She swallowed the spider to catch the fly;
-  I don't know why she swallowed a fly - perhaps she'll die!
-
-  There was an old lady who swallowed a horse...
-  ...She's dead, of course!
-`
+  let prevPrevAnimal = animal
+  for (const i in (previousAnimals.reverse()) ) {
+    const prevAnimal = previousAnimals[i]
+    rows.push(`She swallowed the ${prevPrevAnimal} to catch the ${prevAnimal}${parseInt(i) === (previousAnimals.length - 1) ? ';' : ','}`)
+    prevPrevAnimal = prevAnimal
+  }
+  rows.push(`I don't know why she swallowed a fly - perhaps she'll die!`)
+  return rows
+}
