2ffed5704574635472e26e3b4808d492a9d0b62e
Jan Dvorak <dvorakj@gmail.com>
2021-09-28 22:11:37 +0200

    reafactor: test coverage + configurable options

--- src/__tests__/song.test.ts
+++ src/__tests__/song.test.ts
@@ -2,8 +2,14 @@ import { getSong } from "../song";
 
 describe("song", () => {
   describe("getSong", () => {
-    it("returns some string", () => {
-      const actual = getSong(['fly', 'spider', 'bird', 'cat', 'dog', 'cow']);
+    it("returns song for default animals", () => {
+      const actual = getSong();
+      
+      expect(actual).toMatchSnapshot();
+    });
+
+    it("returns for one animal", () => {
+      const actual = getSong(['bug']);
       
       expect(actual).toMatchSnapshot();
     });
--- src/song.ts
+++ src/song.ts
@@ -1,55 +1,64 @@
-const EXCLAMATIONS = [
-  "",
-  "That wriggled and wiggled and tickled inside her.",
-  "How absurd to swallow a bird.",
-  "Fancy that to swallow a cat!",
-  "What a hog, to swallow a dog!",
-  `I don't know how she swallowed a cow!`,
-];
+const DEFAULT_ANIMALS = ["fly", "spider", "bird", "cat", "dog", "cow"];
 
-const LAST_VERSE = [
-  `There was an old lady who swallowed a horse...`,
-  `...She's dead, of course!`,
-];
+const DEFAULT_OPTIONS = {
+  exclamations: [
+    "",
+    "That wriggled and wiggled and tickled inside her.",
+    "How absurd to swallow a bird.",
+    "Fancy that to swallow a cat!",
+    "What a hog, to swallow a dog!",
+    `I don't know how she swallowed a cow!`,
+  ],
+  lastVerse: [
+    `There was an old lady who swallowed a horse...`,
+    `...She's dead, of course!`,
+  ],
+  firstRow: `There was an old lady who swallowed a`,
+  lastRow: `I don't know why she swallowed a fly - perhaps she'll die!`,
+  mainRow: `She swallowed the $1 to catch the $2`,
+};
 
-const FIRST_ROW = `There was an old lady who swallowed a`;
-
-const LAST_ROW = `I don't know why she swallowed a fly - perhaps she'll die!`;
-
-const MAIN_ROW = `She swallowed the $1 to catch the $2`;
-
-export function getSong(allAnimals: string[]): string {
+export function getSong(
+  allAnimals: string[] = DEFAULT_ANIMALS,
+  customOptions?: Partial<typeof DEFAULT_OPTIONS>
+): string {
+  const options = {...DEFAULT_OPTIONS, ...customOptions}
   let animals = [];
   let verses = [];
 
   allAnimals.forEach((animal, index) => {
     animals = [...animals, animal];
-    verses = [...verses, ...getVerse(index, animal, animals.slice(0, -1)), ""];
+    verses = [
+      ...verses,
+      ...getVerse(index, animal, animals.slice(0, -1), options),
+      "",
+    ];
   });
 
-  return [...verses, ...LAST_VERSE].join("\n").trim();
+  return [...verses, ...options.lastVerse].join("\n").trim();
 }
 
 function getVerse(
   index: number,
   currentAnimal: string,
-  allAnimals: string[]
+  allAnimals: string[],
+  { firstRow, exclamations, mainRow, lastRow }: typeof DEFAULT_OPTIONS
 ): string[] {
   const punctuation = index === 0 ? "." : ";";
-  const rows = [`${FIRST_ROW} ${currentAnimal}${punctuation}`];
+  const rows = [`${firstRow} ${currentAnimal}${punctuation}`];
 
-  const exclamation = EXCLAMATIONS[index];
+  const exclamation = exclamations[index];
   exclamation && rows.push(exclamation);
 
   let prevAnimal = currentAnimal;
   allAnimals.reverse().forEach((animal, index) => {
     const punctuation = index === allAnimals.length - 1 ? ";" : ",";
     rows.push(
-      MAIN_ROW.replace("$1", prevAnimal).replace("$2", animal) + punctuation
+      mainRow.replace("$1", prevAnimal).replace("$2", animal) + punctuation
     );
 
     prevAnimal = animal;
   });
 
-  return [...rows, LAST_ROW];
+  return [...rows, lastRow];
 }
