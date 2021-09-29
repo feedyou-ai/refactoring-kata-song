22490fb8f8fea32bcfee478164328aef3a30e880
Jan Dvorak <dvorakj@gmail.com>
2021-09-29 21:50:58 +0200

    chore: format index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
-import { getSong } from './src/song'
+import { getSong } from "./src/song";
 
-console.log(getSong(['dítě', 'slona'], {exclamations:[], firstRow: 'Bába spolkla', lastRow: 'Nevím proč spolkla dítě', lastVerse: ['Spolkla auto...','... a už je mrtvá!'], mainRow: 'Spolkla $1 aby chytila $2'}))
+console.log(
+  getSong(["dítě", "slona", "chobotnice"], {
+    exclamations: ["", "!!!", "???"],
+    firstRow: "Bába spolkla",
+    lastRow: "Nevím proč spolkla dítě",
+    lastVerse: ["Spolkla auto...", "... a už je mrtvá!"],
+    mainRow: "Spolkla $1 aby chytila $2",
+  })
+);
